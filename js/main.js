c0b373dabb3a02911607caaf268d21ca40c124f4
Bendry Lakburlawal <bendrylakburlawal01@gmail.com>
2023-12-30 00:07:06 +0700

    conditional and dom for login

--- js/main.js
+++ js/main.js
@@ -1,3 +1,45 @@
-alert('Belajar JS');
+function checkUserData(username, password){
+    var usernameDb = 'aku@gmail.com';
+    var passwordDb = '1234';
 
-console.log('Bendry Belajar');
+    var validasiUSer = username == usernameDb;
+    var validasiPass = password == passwordDb;
+
+    if(!validasiUSer){
+       alert('Username anda salah');
+    }
+
+    if(!validasiPass){
+        alert('Password anda salah');
+    }
+
+    if(validasiPass && validasiUSer){
+        alert('Anda berhasil login');
+    }
+}
+
+// console.log('Bendry Belajar');
+
+// //array
+// var telurArray = ['Gado-gado', 'Ketoptrak', 'Omlet'];
+// telurArray.splice(0, 1);
+// telurArray.push('Dadar');
+// console.log(telurArray[2]);
+// console.log(telurArray);
+
+// //object
+// var rumah = {
+//     lebarTanah : 50, 
+//     lebarBanguanan : 100,
+//     warna : 'hitam',
+//     kids : [
+//         'Arya',
+//         'Juan',
+//         'Fabil',
+//         'Ririn'
+//     ]
+// }
+
+// console.log(rumah);
+// console.log(rumah.warna);
+// console.log(rumah.kids[3]);
