f16846047b1b3db37bb30e13142e567cc5a0e87d
Bendry Lakburlawal <bendrylakburlawal01@gmail.com>
2023-12-30 19:36:37 +0700

    done

--- js/main.js
+++ js/main.js
@@ -1,45 +1,53 @@
-function checkUserData(username, password){
-    var usernameDb = 'aku@gmail.com';
-    var passwordDb = '1234';
+let myArray = JSON.parse(localStorage.getItem('myArray')) || [];
+renderCars();
 
-    var validasiUSer = username == usernameDb;
-    var validasiPass = password == passwordDb;
+function saveCars() {
+    const name = document.getElementById('carsName');
+    const price = document.getElementById('carsPrice');
+    const image = document.getElementById('carsImage');
 
-    if(!validasiUSer){
-       alert('Username anda salah');
-    }
+    const nameValue = name.value;
+    const priceValue = price.value;
+    const imageValue = image.value;
 
-    if(!validasiPass){
-        alert('Password anda salah');
-    }
+    myArray.push({
+        nameValue,
+        priceValue,
+        imageValue
+    });
 
-    if(validasiPass && validasiUSer){
-        alert('Anda berhasil login');
-    }
+    name.value = '';
+    price.value = '';
+    image.value = '';
+
+    renderCars();
+    localStorage.setItem('myArray', JSON.stringify(myArray));
 }
 
-// console.log('Bendry Belajar');
+function renderCars() {
+    let cars = document.getElementById('carsList');
+    let listCars = '';
 
-// //array
-// var telurArray = ['Gado-gado', 'Ketoptrak', 'Omlet'];
-// telurArray.splice(0, 1);
-// telurArray.push('Dadar');
-// console.log(telurArray[2]);
-// console.log(telurArray);
+    for (let i = 0; i < myArray.length; i++) {
+        const theCars = myArray[i];
+        const { nameValue, priceValue, imageValue } = theCars;
 
-// //object
-// var rumah = {
-//     lebarTanah : 50, 
-//     lebarBanguanan : 100,
-//     warna : 'hitam',
-//     kids : [
-//         'Arya',
-//         'Juan',
-//         'Fabil',
-//         'Ririn'
-//     ]
-// }
-
-// console.log(rumah);
-// console.log(rumah.warna);
-// console.log(rumah.kids[3]);
+        const html = `
+            <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                <img src="` + imageValue + `" alt="" class="img-fluid">
+                <h3 class="mt-3" id="carName2">` + nameValue + `</h3>
+                <p>$` + priceValue + `</p>
+                <p>
+                    <a href="#" class="btn-view btn btn-primary w-100">View Details</a>
+                    <a href="#" class="btn-book mt-2 btn btn-secondary w-100">Book Now</a>
+                </p>
+                <button onclick="
+                    myArray.splice(${i}, 1);
+                    renderCars();
+                    localStorage.setItem('myArray', JSON.stringify(myArray));
+                " class="btn btn-danger w-100">Delete Car</button>
+            </div>`;
+        listCars += html;
+    }
+    cars.innerHTML = listCars;
+}
